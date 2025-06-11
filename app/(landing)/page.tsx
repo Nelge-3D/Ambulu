19ce4560a084899d17cbe3be254186ff0e827776
Nelge-3D <louisgeriel.ngabi@gmail.com>
2025-06-11 19:05:07 +0300

    landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,21 @@
+import About from "./About";
+import FloatingMenu from "./FloatingMenu";
+import Hero from "./Hero";
+import Contact from "./Contact";
+import Demo from "./Demo";
+import Footer from "./Footer";
+
 export default function page () {
     return [
-        <div>landing page</div>
+        <main className="bg-background text-text font-sans">
+            <FloatingMenu/>
+            <Hero/>
+            <About/>
+            <Contact/>
+            <Demo/>
+            <Footer/>
+
+
+        </main>
     ]
 }
