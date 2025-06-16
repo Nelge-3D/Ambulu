7ecf2585de7b3b0dd8e68c4d95dafacabb071053
Nelge-3D <louisgeriel.ngabi@gmail.com>
2025-06-16 13:55:44 +0300

    Rajoute de la landing page manquante

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,25 @@
+import About from "./About";
+import FloatingMenu from "./FloatingMenu";
+import Hero from "./Hero";
+import Contact from "./Contact";
+import Demo from "./Demo";
+import Footer from "./Footer";
+import Nav from "./Nav";
+
 export default function page () {
+
     return (
-        <div>landing page</div>
+        <main className="bg-background text-text font-sans">
+            <FloatingMenu/>
+            <Nav/>
+            <Hero/>
+            <About/>
+            <Contact/>
+            <Demo/>
+            <Footer/>
+
+
+        </main>
     )
+
 }
